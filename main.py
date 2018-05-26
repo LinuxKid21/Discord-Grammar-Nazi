9b9e1432fe0a8edd30811623f796fe162d1b5b6d
LinuxKid21 <linuxkid21@gmail.com>
2018-05-25 17:59:36 -0700

    moved grammar bot into another function

--- main.py
+++ main.py
@@ -29,31 +29,38 @@ async def on_ready():
     print('Logged in ' + str(client.user.name) + ' - ' +
           str(client.user.id) + '\n' + 'Version ' + str(discord.__version__))
 
+
+def handle_grammar(message):
+    matches = grammar_en.check(message.content)
+    sentences = []
+    for match in matches:
+        bad_word = message.content[match.offset:match.offset + match.errorlength]
+        if(match.ruleId == 'UPPERCASE_SENTENCE_START'):
+            sentences.append('Wow. I can\'t believe you didn\'t start your sentence with a capital letter.')
+        elif(match.ruleId == 'MORFOLOGIK_RULE_EN_US'): # spelling errors
+            sentences.append('Ugh. The word \'' + bad_word + '\' is not spelled correctly.')
+        elif(match.ruleId == 'IT_VBZ'): # this are a problem <- fixes that sort of issue
+            sentences.append('Subject verb disagreement! Did you mean to use \'' + match.replacements[0] + '\' instead? (hint: you did)')
+        elif(match.ruleId in ['IT_IS', 'EN_CONTRACTION_SPELLING']): # its when should be it's and that when should be that's
+            sentences.append('Missing an apostrophe here: \'' + bad_word + '\'. Fix it.')
+        elif(match.ruleId == 'PROGRESSIVE_VERBS'): # ???
+            sentences.append('This word is not usually used in the progressive form.' +
+                'I know you don\'t know what that means. http://lmgtfy.com/?q=progressive+form')
+        elif(match.ruleId == 'PERS_PRONOUN_AGREEMENT_SENT_START'): # I are should be I am
+            sentences.append('really? That verb does not agree with \'' + bad_word + '\'. You clearly meant \'' + match.replacements[0] + '\'')
+        else:
+            sentences.append([match])
+    return sentences
+
  
 @client.event
 async def on_message(message):
     if(message.author.bot):
         return
 
-    matches = grammar_en.check(message.content)
-    for match in matches:
-        bad_word = message.content[match.offset:match.offset + match.errorlength]
-        if(match.ruleId == 'UPPERCASE_SENTENCE_START'):
-            await message.channel.send('Wow. I can\'t believe you didn\'t start your sentence with a capital letter.')
-        elif(match.ruleId == 'MORFOLOGIK_RULE_EN_US'): # spelling errors
-            await message.channel.send('Ugh. The word \'' + bad_word + '\' is not spelled correctly.')
-        elif(match.ruleId == 'IT_VBZ'): # this are a problem <- fixes that sort of issue
-            await message.channel.send('Subject verb disagreement! Did you mean to use \'' + match.replacements[0] + '\' instead? (hint: you did)')
-        elif(match.ruleId in ['IT_IS', 'EN_CONTRACTION_SPELLING']): # its when should be it's and that when should be that's
-            await message.channel.send('Missing an apostrophe here: \'' + bad_word + '\'. Fix it.')
-        elif(match.ruleId == 'PROGRESSIVE_VERBS'): # ???
-            await message.channel.send('This word is not usually used in the progressive form.' +
-                'I know you don\'t know what that means. http://lmgtfy.com/?q=progressive+form')
-        elif(match.ruleId == 'PERS_PRONOUN_AGREEMENT_SENT_START'): # I are should be I am
-            await message.channel.send('really? That verb does not agree with \'' + bad_word + '\'. You clearly meant \'' + match.replacements[0] + '\'')
-        else:
-            await message.channel.send([match])
-
+    sentences = handle_grammar(message)
+    for sentence in sentences:
+        await message.channel.send(sentence)
 
 
     if "thesaurus " in message.content:
